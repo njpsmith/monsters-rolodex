4bd0fc35d9f5b80bc75f0ec4fe88721cc4e77d60
Nick Smith <>
2020-04-22 19:38:50 +0100

    Updates

---
+++ precache-manifest.28648cada965fda57215865f46029c41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3621d8de3dd9de26b2ab7852980cd09c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c85109a31f3f6df4c767",
+    "url": "/monsters/static/css/main.e3fb7a53.chunk.css"
+  },
+  {
+    "revision": "20b1912bf242704f9f11",
+    "url": "/monsters/static/js/2.0a6b9ac9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.0a6b9ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85109a31f3f6df4c767",
+    "url": "/monsters/static/js/main.6b06632c.chunk.js"
+  },
+  {
+    "revision": "360bbdfb15409043b41d",
+    "url": "/monsters/static/js/runtime-main.6c4a148e.js"
+  }
+]);
